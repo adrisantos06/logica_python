fa58929e3b50e2e6b2162f664485d09ef619602a
adrisantos06 <adriellesantosx120@gmail.com>
2026-06-24 08:33:47 -0300

    Update ex0012.py

--- aula-01/aula-03/ex0012.py
+++ aula-01/aula-03/ex0012.py
@@ -3,4 +3,4 @@ desconto = preco * 5 / 100
 novo_preco = preco - desconto
 print(f'\nPreço original: R$ {preco:.2f}')
 print(f"Desconto (5%): R$ {desconto:.2f}")
-print(f'Preço final:        R$ {novo_preco:.2f}')
+print(f'Preço final: R$ {novo_preco:.2f}')
