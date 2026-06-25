dbf745d0ef3e5f22e32bce6ef9aa3b47ed8b9175
adrisantos06 <adriellesantosx120@gmail.com>
2026-06-25 09:47:27 -0300

    feat(ex0019.py):adicionar o exercicio 0019

---
+++ aula-05/ex0019.py
@@ -0,0 +1,11 @@
+from random import choice
+
+n1 = str(input(" primeiro aluno: "))
+n2= str(input(" segundo aluno: "))
+n3 = str(input(" terceiro aluno: "))
+n4 = str(input(" quarto aluno: " ))
+
+alunos = [n1,n2,n3,n4]
+escolhido = choice(alunos)
+
+print(f' o aluno escolhido foi: {escolhido}')
